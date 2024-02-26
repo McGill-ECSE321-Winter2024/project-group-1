1a8d460b3f672209338ddebd4c8149ee20717089
andrewnemr123 <143835486+andrewnemr123@users.noreply.github.com>
2024-02-26 13:00:17 -0500

    worked a bit before class

--- sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/service/CustomerService.java
+++ sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/service/CustomerService.java
@@ -1,5 +1,46 @@
 package ca.mcgill.ecse321.sportcenter.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import ca.mcgill.ecse321.sportcenter.dao.CustomerRepository;
+import ca.mcgill.ecse321.sportcenter.model.Customer;
+
 public class CustomerService {
     
+    @Autowired
+    CustomerRepository customerRepository;
+
+    public List<Customer> getCustomers() {
+        return toList(customerRepository.findAll());
+    }
+
+    /**
+     * Converts an Iterable to a List.
+     * 
+     * @param iterable the Iterable to convert
+     * @return a List containing the elements of the Iterable
+     */
+    private <T> List<T> toList(Iterable<T> iterable) {
+        List<T> list = new ArrayList<>();
+        iterable.forEach(list::add);
+        return list;
+    }
+    /**
+     * Get a customer by its accountRole Id (primary key)
+     * @param accountRoleId
+     * @return Customer
+     * 
+     */
+
+    @Transactional
+    public Customer getCustomer(int accountRoleId) {
+        if (accountRoleId < 0) {
+            throw new IllegalArgumentException("AccountRoleId cannot be negative!");
+        }
+        return customerRepository.findAccountRoleByAccountRoleId(accountRoleId);
+    }
 }
--- sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/service/InstructorService.java
+++ sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/service/InstructorService.java
@@ -1,5 +1,39 @@
 package ca.mcgill.ecse321.sportcenter.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import ca.mcgill.ecse321.sportcenter.dao.InstructorRepository;
+import ca.mcgill.ecse321.sportcenter.model.Instructor;
+
 public class InstructorService {
+    @Autowired
+    InstructorRepository instructorRepository;
+
+    public List<Instructor> getInstructors() {
+        return toList(instructorRepository.findAll());
+    }
+
+    private List<Instructor> toList(Iterable<Instructor> iterable) {
+        List<Instructor> list = new ArrayList<>();
+        iterable.forEach(list::add);
+        return list;
+    }
+    /**
+     * Get an instructor by its accountRole Id (primary key)
+     * @param accountRoleId
+     * @return Instructor
+     * 
+     */
+    @Transactional
+    public Instructor getInstructor(int accountRoleId) {
+        if (accountRoleId < 0) {
+            throw new IllegalArgumentException("AccountRoleId cannot be negative!");
+        }
+        return instructorRepository.findAccountRoleByAccountRoleId(accountRoleId);
+    }
     
 }
