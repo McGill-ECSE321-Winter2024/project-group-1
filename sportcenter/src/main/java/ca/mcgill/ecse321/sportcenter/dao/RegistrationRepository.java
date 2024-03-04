7bfa1ca1b8411f9156f0176a486719a4d13b9424
emilieruel <emilie.ruel2@mail.mcgill.ca>
2024-03-04 17:18:45 -0500

    Registration services start

--- sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/dao/RegistrationRepository.java
+++ sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/dao/RegistrationRepository.java
@@ -1,6 +1,10 @@
 package ca.mcgill.ecse321.sportcenter.dao;
 
 import org.springframework.data.repository.CrudRepository;
+
+import ca.mcgill.ecse321.sportcenter.controller.ScheduledActivity;
+import ca.mcgill.ecse321.sportcenter.controller.CustomerDto;
+import ca.mcgill.ecse321.sportcenter.model.Customer;
 import ca.mcgill.ecse321.sportcenter.model.Registration;
 
 /**
@@ -10,4 +14,10 @@ import ca.mcgill.ecse321.sportcenter.model.Registration;
 public interface RegistrationRepository extends CrudRepository<Registration, Integer>{
 
     public Registration findRegistrationByRegId(int regId); //regId is the Primary Key
+
+    public Registration findRegistrationByCustomerAndScheduledActivity(Customer customer, ScheduledActivity scheduledActivity);
+
+    public Registration findBySchuledActivity(ScheduledActivity scheduledActivity);
+
+    public Registration findByCustomer(Customer customer);
 } 
