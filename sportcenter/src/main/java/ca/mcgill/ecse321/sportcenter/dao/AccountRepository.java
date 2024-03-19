fc697225a709a59f1c30bcaecfdf6c98c251eaad
Patrick Zakaria <73553056+Nomber1-1@users.noreply.github.com>
2024-03-19 18:50:50 -0400

    #40 Fixed Service methods and Updated account repository

--- sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/dao/AccountRepository.java
+++ sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/dao/AccountRepository.java
@@ -5,12 +5,12 @@ import ca.mcgill.ecse321.sportcenter.model.Account;
 
 /**
  * @author Anslean Albert Jeyaras (GumballGB on GitHub) and Patrick Zakaria
- * dao and CRUD for Account
+ *         dao and CRUD for Account
  */
-public interface AccountRepository extends CrudRepository<Account, Integer>{
-    
-    public Account findAccountByAccountId(int accoundId); //accoundId is the Primary Key
-    
-    public Account findAccountByUsername(String username);//check with TA if we want an accountId or username to be the primary key
+public interface AccountRepository extends CrudRepository<Account, Integer> {
+
+    public Account findAccountByAccountId(int accoundId); // accoundId is the Primary Key
+
+    public Account findAccountByUsername(String username); // username is the unique identifier for an account
 
 }
--- sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/service/AccountService.java
+++ sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/service/AccountService.java
@@ -10,40 +10,47 @@ import org.springframework.transaction.annotation.Transactional;
 import ca.mcgill.ecse321.sportcenter.dao.AccountRepository;
 import ca.mcgill.ecse321.sportcenter.model.Account;
 
+/**
+ * Service class for the Account entity
+ * 
+ * @author Andrew Nemr
+ */
 @Service
 public class AccountService {
     @Autowired
     AccountRepository accountRepository;
-    
-    private <T> List<T> toList(Iterable<T> iterable) {
-        List<T> resultList = new ArrayList<T>();
-        for (T t : iterable) {
-            resultList.add(t);
-        }
-        return resultList;
-    }
 
     /**
      * Create an account
+     * 
      * @param username
      * @param password
      * @return Account
-     * @author Andrew Nemr
      */
     @Transactional
     public Account createAccount(String username, String password) {
-
-        Account account = new Account();
+        if (username == null || username.trim().length() == 0) {
+            throw new IllegalArgumentException("Username cannot be empty!");
+        }
+        if (password == null || password.trim().length() == 0) {
+            throw new IllegalArgumentException("Password cannot be empty!");
+        }
+        Account account;
+        if (accountRepository.findAccountByUsername(username) != null) {
+            throw new IllegalArgumentException("Username already exists!");
+        }
+        account = new Account();
         account.setUsername(username);
         account.setPassword(password);
         accountRepository.save(account);
         return account;
     }
+
     /**
      * Get an account by its account ID (primary key)
+     * 
      * @param accountId
      * @return Account
-     * 
      */
     @Transactional
     public Account getAccountById(int accountId) {
@@ -53,71 +60,10 @@ public class AccountService {
         }
         return account;
     }
-    /**
-     * Validate an account's username and password
-     * @param username
-     * @param password
-     */
 
-   public void validateAccount(String username, String password) {
-
-        Account account = accountRepository.findAccountByUsername(username);
-        if (account != null) {
-            throw new IllegalArgumentException("Username already exists!");
-        }
-        if (!account.getPassword().equals(password)) {
-            throw new IllegalArgumentException("Password is incorrect");
-        }
-        String error = "";
-        if (username == null || username.trim().length() == 0) {
-            error = error + "Username cannot be empty! ";
-        }
-        if (password == null || password.trim().length() == 0) {
-            error = error + "Password cannot be empty! ";
-        }
-        if (error.length() > 0) {
-            throw new IllegalArgumentException(error);
-        }
-    }
-    /**
-     * Get all accounts
-     * @return List<Account>
-     * 
-     * @Author Andrew Nemr
-     */
-    @Transactional
-    public List<Account> getAllAccounts() {
-        return toList(accountRepository.findAll());
-    }
-
-    /**
-     * Delete an account by its account ID (primary key)
-     * @param accountId
-     */
-    @Transactional
-    public void deleteAccount(int accountId) {
-        accountRepository.deleteById(accountId);
-    }
-    /**
-     * Update an account's username and password
-     * @param accountId
-     * @param username
-     * @param password
-     */
-    @Transactional
-    public Account updateAccount(String oldUsername, String username, String password) {
-        Account account = accountRepository.findAccountByUsername(oldUsername);
-        if (account == null) {
-            throw new IllegalArgumentException("Account does not exist");
-        }
-        validateAccount(username, password);
-        account.setUsername(username);
-        account.setPassword(password);
-        accountRepository.save(account);
-        return account;
-    }
     /**
      * Get an account by its username
+     * 
      * @param username
      * @return Account
      */
@@ -129,8 +75,82 @@ public class AccountService {
         }
         return account;
     }
+
+    /**
+     * Helper function to turn an iterable into a list
+     * 
+     * @param Iterable<T>
+     * @return List<T>
+     */
+    private <T> List<T> toList(Iterable<T> iterable) {
+        List<T> resultList = new ArrayList<T>();
+        for (T t : iterable) {
+            resultList.add(t);
+        }
+        return resultList;
+    }
+
+    /**
+     * Get all accounts
+     * 
+     * @return List<Account>
+     * @author Andrew Nemr
+     */
+    @Transactional
+    public List<Account> getAllAccounts() {
+        return toList(accountRepository.findAll());
+    }
+
+    /**
+     * Update an account's username and password
+     * 
+     * @param accountId
+     * @param newUsername
+     * @param newPassword
+     * @return Account
+     */
+    @Transactional
+    public Account updateAccount(String oldUsername, String newUsername, String newPassword) {
+        if (oldUsername == null || oldUsername.trim().length() == 0) {
+            throw new IllegalArgumentException("Username cannot be empty");
+        }
+        if (newUsername == null || newUsername.trim().length() == 0) {
+            throw new IllegalArgumentException("Username cannot be empty");
+        }
+        if (newPassword == null || newPassword.trim().length() == 0) {
+            throw new IllegalArgumentException("Password cannot be empty");
+        }
+
+        Account account = accountRepository.findAccountByUsername(oldUsername);
+        if (account == null) {
+            throw new IllegalArgumentException("Account with the old Username does not exist");
+        }
+        if (accountRepository.findAccountByUsername(newUsername) != null) {
+            throw new IllegalArgumentException("Account with the new Username already exists");
+        }
+        account.setUsername(newUsername);
+        if (!account.getPassword().equals(newPassword)) {
+            throw new IllegalArgumentException("Password is incorrect");
+        }
+
+        account.setPassword(newPassword);
+        accountRepository.save(account);
+        return account;
+    }
+
+    /**
+     * Delete an account by its account ID (primary key)
+     * 
+     * @param accountId
+     */
+    @Transactional
+    public void deleteAccount(int accountId) {
+        accountRepository.deleteById(accountId);
+    }
+
     /**
      * Delete all accounts
+     * 
      * @return void
      */
     @Transactional
@@ -138,49 +158,24 @@ public class AccountService {
         accountRepository.deleteAll();
     }
 
-    // check group 14 of winter 2022 good ideas for checking for stuff
-
-    //check for email uniqueness
-    public boolean checkUsernameUniqueness(String username) {
-        for (Account account : getAllAccounts()) {
-            if (account.getUsername().equals(username)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    //check for password correctness
-    public boolean checkPasswordCorrectness(String username, String password) {
-        for (Account account : getAllAccounts()) {
-            if (account.getUsername().equals(username)) {
-                if (account.getPassword().equals(password)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    //check for account existence
-    public boolean checkAccountExistence(String username) {
-        for (Account account : getAllAccounts()) {
-            if (account.getUsername().equals(username)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    //check all input parameters for account creation
-    public boolean checkAccountCreation(String username, String password) {
+    /**
+     * Login function
+     * 
+     * @param username
+     * @param password
+     * @return Account
+     */
+    public boolean login(String username, String password) {
         if (username == null || username.trim().length() == 0) {
-            return false;
+            throw new IllegalArgumentException("Username cannot be empty");
         }
         if (password == null || password.trim().length() == 0) {
-            return false;
+            throw new IllegalArgumentException("Password cannot be empty");
         }
-        return true;
+        Account account = accountRepository.findAccountByUsername(username);
+        if (account == null) {
+            throw new IllegalArgumentException("Account does not exist");
+        }
+        return account.getPassword().equals(password);
     }
-
 }
