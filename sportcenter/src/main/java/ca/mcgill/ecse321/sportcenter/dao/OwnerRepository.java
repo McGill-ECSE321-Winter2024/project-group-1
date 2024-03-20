77d618dba1a23b52d874eb1f41899bb4be6d6be9
mathias-pl <mathias.pachecolemina@mail.mcgill.ca>
2024-03-20 01:46:21 -0400

    Owner Testing
    Issue #54 - Complete Owner Testing
    Service is done, some methods have been added

--- sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/dao/OwnerRepository.java
+++ sportcenter/src/main/java/ca/mcgill/ecse321/sportcenter/dao/OwnerRepository.java
@@ -5,10 +5,12 @@ import ca.mcgill.ecse321.sportcenter.model.Owner;
 
 /**
  * @author Anslean Albert Jeyaras (GumballGB on GitHub) and Patrick Zakaria
- * dao and CRUD for Owner
+ *         dao and CRUD for Owner
  */
-public interface OwnerRepository extends CrudRepository<Owner, Integer>{
- 
-    public Owner findAccountRoleByAccountRoleId(int accountRoleId); //Account role is parent class. Primary Key accountRoleId
+public interface OwnerRepository extends CrudRepository<Owner, Integer> {
 
+    public Owner findAccountRoleByAccountRoleId(int accountRoleId); // Account role is parent class. Primary Key
+                                                                    // accountRoleId
+
+    public Owner findOwnerByAccountRoleId(int accountRoleId); // Account role is parent class. Primary Key accountRoleId
 }
--- sportcenter/src/test/java/ca/mcgill/ecse321/sportcenter/repository/TestOwnerRepository.java
+++ sportcenter/src/test/java/ca/mcgill/ecse321/sportcenter/repository/TestOwnerRepository.java
@@ -1,5 +1,10 @@
 package ca.mcgill.ecse321.sportcenter.repository;
 
+import ca.mcgill.ecse321.sportcenter.model.Customer;
+
+/**
+ * @author Mathias Pacheco Lemina
+ */
 public class TestOwnerRepository {
-    
+
 }
