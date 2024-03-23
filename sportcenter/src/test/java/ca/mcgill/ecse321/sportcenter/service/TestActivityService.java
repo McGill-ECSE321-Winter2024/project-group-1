f5dda4317ae83d1ddb2c0dc4ca8a3349f42b649f
Patrick Zakaria <73553056+Nomber1-1@users.noreply.github.com>
2024-03-23 17:53:20 -0400

    Update TestActivityService.java

--- sportcenter/src/test/java/ca/mcgill/ecse321/sportcenter/service/TestActivityService.java
+++ sportcenter/src/test/java/ca/mcgill/ecse321/sportcenter/service/TestActivityService.java
@@ -1,8 +1,6 @@
 package ca.mcgill.ecse321.sportcenter.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
