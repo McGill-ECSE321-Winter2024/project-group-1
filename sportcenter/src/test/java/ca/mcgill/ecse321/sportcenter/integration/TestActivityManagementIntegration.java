48abf79724de8c96bbb769b87fa0aabf4a9bd298
Anslean <92391858+GumballGB@users.noreply.github.com>
2024-03-24 00:37:55 -0400

    Create Integration File #48
    TestActivityManagementIntegration.java created

---
+++ sportcenter/src/test/java/ca/mcgill/ecse321/sportcenter/integration/TestActivityManagementIntegration.java
@@ -0,0 +1,51 @@
+package ca.mcgill.ecse321.sportcenter.integration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+
+import ca.mcgill.ecse321.sportcenter.dao.AccountRepository;
+import ca.mcgill.ecse321.sportcenter.dao.ActivityRepository;
+import ca.mcgill.ecse321.sportcenter.dao.CustomerRepository;
+import ca.mcgill.ecse321.sportcenter.dao.InstructorRepository;
+
+
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class TestActivityManagementIntegration {
+
+
+
+    @Autowired
+    private ActivityRepository activityRepository;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+
+    @BeforeEach
+    @AfterEach
+    public void clearDatabase() {
+		activityRepository.deleteAll();
+        accountRepository.deleteAll();
+	}
+
+
+    
+
+
+
+
+
+}
