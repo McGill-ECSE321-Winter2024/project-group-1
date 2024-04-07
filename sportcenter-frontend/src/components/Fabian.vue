84249b5e527aed0df3f64eec3e13f179f9b5397e
Fabian Saldana <114820909+FabianSaldana@users.noreply.github.com>
2024-04-06 23:04:37 -0400

    #63 Updated the page for instructor

---
+++ sportcenter-frontend/src/components/Fabian.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="fabian">
+        <div class = "propose-activity">
+            <h2> Left Section</h2>
+            <div class="left-text-fields"> 
+                <input type="text" placeholder="Field 1">
+                <br>
+                <input type="text" placeholder="Field 2">
+                <br>
+                <input type="text" placeholder="Field 3">
+            </div>
+            <button> Submit Left Section</button>
+        </div>
+        <div class = "schedule-activity">
+            <h2> Right Section</h2>
+            <div class="right-text-fields">
+                <input type="text" placeholder="Field 4">
+                <br>
+                <input type="text" placeholder="Field 5">
+                <br>
+                <input type="text" placeholder="Field 6">
+                <br>
+                <input type="text" placeholder="Field 7">
+                <br>
+                <input type="text" placeholder="Field 8">
+            </div>
+            <button> Submit Right Section</button>
+        </div>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'instructor',
+};
+
+</script>
+  
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .hello {
+        display: flex;
+}
+  
+.left,
+.right {
+    flex: 1;
+}
+  
+h2 {
+    font-weight: normal;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+
+a {
+    color: #42b983;
+}
+</style>
+  
