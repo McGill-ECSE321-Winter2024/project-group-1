556f71458fd6c3c4aca6cbf256fd25590d87a584
emilieruel <emilie.ruel2@mail.mcgill.ca>
2024-04-10 11:34:51 -0400

    new activity type and scheduled activity #63

--- sportcenter-frontend/src/components/activity/ProposeActivity.vue
+++ sportcenter-frontend/src/components/activity/ProposeActivity.vue
@@ -1,76 +1,98 @@
 <template>
-    <div class="Fabian" id="mainContainer">
-        <h1>Propose an activity</h1>
-        
-        <VBox id="verticalContainer">
-            
-            <input id="inputBox" type="text" placeholder="Activity name" v-model="activityName">
-            <input id="inputBox" type="text" placeholder="Description" v-model="description">
-            <input id="inputBox" type="text" placeholder="Subcategory" v-model="subcategory">
-            <!--<button id="mainButton" @click="created()">GetActivities</button>-->
-            <button id="mainButton" @click="submitProposeActivity()">Propose to the owner</button>
-        </VBox>
-    </div>
+  <div class="Fabian" id="mainContainer">
+    <h1>Propose an activity</h1>
+
+    <VBox id="verticalContainer">
+      <input
+        id="inputBox"
+        type="text"
+        placeholder="Activity name"
+        v-model="activityName"
+      />
+      <input
+        id="inputBox"
+        type="text"
+        placeholder="Description"
+        v-model="description"
+      />
+      <input
+        id="inputBox"
+        type="text"
+        placeholder="Subcategory"
+        v-model="subcategory"
+      />
+      <!--<button id="mainButton" @click="created()">GetActivities</button>-->
+      <button id="mainButton" @click="submitProposeActivity()">
+        Propose to the owner
+      </button>
+    </VBox>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 import config from "../../../config";
 
-const frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
-const backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+const frontendUrl = "http://" + config.dev.host + ":" + config.dev.port;
+const backendUrl =
+  "http://" + config.dev.backendHost + ":" + config.dev.backendPort;
 
 const AXIOS = axios.create({
-    baseURL: backendUrl,
-    headers: { 'Access-Control-Allow-Origin': frontendUrl }
-})
+  baseURL: backendUrl,
+  headers: { "Access-Control-Allow-Origin": frontendUrl },
+});
 
 export default {
-    name: "Activities",
-    data () {
-        return {
-            activities: [],
-            activityName: null,
-            description: null,
-            subcategory: null,
-            date: null,
-            startTime: null,
-            endTime: null,
-            accountRoleId: null,
-            activityName2: null,
-            capacity: null
-        };
+  name: "Activities",
+  data() {
+    return {
+      activities: [],
+      activityName: null,
+      description: null,
+      subcategory: null,
+      date: null,
+      startTime: null,
+      endTime: null,
+      accountRoleId: null,
+      activityName2: null,
+      capacity: null,
+    };
+  },
+  methods: {
+    async submitProposeActivity() {
+      const newActivity = {
+        activityName: this.activityName,
+        description: this.description,
+        subcategory: this.subcategory,
+      };
+      try {
+        const response = await AXIOS.post(
+          "/createActivity/" +
+            activityName +
+            "/" +
+            description +
+            "/" +
+            subcategory
+        );
+        console.log(response.data);
+        this.clearInputs();
+      } catch (error) {
+        alert("Problem proposing activity. Please try again.");
+      }
     },
-    methods:{
-        async submitProposeActivity(){
-            const newActivity = {
-                activityName: this.activityName,
-                description: this.description,
-                subcategory: this.subcategory
-            };
-            try{
-                const response = await AXIOS.post('/createActivity/' + this.activityName + '/' + this.description + '/' + this.subcategory);
-                this.activities.push(response.data);
-                this.clearInputs();
-            } catch(error){
-                alert(error.message);
-            }
-        },
-        clearInputs(){
-            this.activityName = null;
-            this.description = null;
-            this.subcategory = null;
-            this.date = null;
-            this.startTime = null;
-            this.endTime = null;
-            this.accountRoleId = null;
-            this.activityName2 = null;
-            this.capacity = null;
-        }
-
-    }
+    clearInputs() {
+      this.activityName = null;
+      this.description = null;
+      this.subcategory = null;
+      this.date = null;
+      this.startTime = null;
+      this.endTime = null;
+      this.accountRoleId = null;
+      this.activityName2 = null;
+      this.capacity = null;
+    },
+  },
 };
-
 </script>
 
 <style scoped src="../../assets/main.css"></style>
--- sportcenter-frontend/src/components/activity/ScheduleActivity.vue
+++ sportcenter-frontend/src/components/activity/ScheduleActivity.vue
@@ -1,119 +1,101 @@
 <template>
-    <div class="Fabian" id="mainContainer">
-        <h1>Schedule an activity</h1>
+  <div class="ScheduleAcivity" id="mainContainer">
+    <h1>Schedule an activity</h1>
 
-        <Vbox id="verticalContainer">
-            <HBox id="horizontalContainer">
-                <input id="datePickerInput" type="date" name="scheduled">
-                <h2 style="align-self:center;">from</h2>
-                <input id="datePickerInput" type="time" name="startTime">
-                <h2 style="align-self:center;">to</h2>
-                <input id="datePickerInput" type="time" name="endTime">
-            </HBox>
-            <select v-model="selectAccount">
-                <option v-for="account in accounts" :key="account.accountRoleId" :value="account.accountRoleId">{{account.accountRoleId}}</option>>
-            </select>
-            <br>
-            <select v-model="selectActivity">
-                <option v-for="activity in activities" :key="activity.name" :value="activity.name">{{activity.name}}</option>>
-            </select>
-            <!--
+    <Vbox id="verticalContainer">
+      <HBox id="horizontalContainer">
+        <input id="datePickerInput" type="date" name="scheduled" />
+        <h2 style="align-self: center">from</h2>
+        <input id="datePickerInput" type="time" name="startTime" />
+        <h2 style="align-self: center">to</h2>
+        <input id="datePickerInput" type="time" name="endTime" />
+      </HBox>
+      <select v-model="selectAccount">
+        <option
+          v-for="account in accounts"
+          :key="account.accountRoleId"
+          :value="account.accountRoleId"
+        >
+          {{ account.accountRoleId }}
+        </option>
+        >
+      </select>
+      <br />
+      <select v-model="selectActivity">
+        <option
+          v-for="activity in activities"
+          :key="activity.name"
+          :value="activity.name"
+        >
+          {{ activity.name }}
+        </option>
+        >
+      </select>
+      <!--
             <input id="inputBox" type="text" placeholder="Account Role Id" v-model="accountRoleId">
             <input id="inputBox" type="text" placeholder="Activity Name" v-model="activityName2">
             -->
-            <input id="inputBox" type="text" placeholder="Capacity" v-model="capacity">
-            
-            <button id="mainButton" @click="submitScheduleActivity()">Schedule Activity</button>
-        </Vbox>
-    </div>
+      <input
+        id="inputBox"
+        type="text"
+        placeholder="Capacity"
+        v-model="capacity"
+      />
+
+      <button id="mainButton" @click="submitScheduleActivity()">
+        <b>Schedule Activity</b>
+      </button>
+    </Vbox>
+  </div>
 </template>
-  
+
 <script>
 import axios from "axios";
 import config from "../../../config";
 
-const frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
-const backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+const frontendUrl = "http://" + config.dev.host + ":" + config.dev.port;
+const backendUrl =
+  "http://" + config.dev.backendHost + ":" + config.dev.backendPort;
 
 const AXIOS = axios.create({
-    baseURL: backendUrl,
-    headers: { 'Access-Control-Allow-Origin': frontendUrl }
-})
+  baseURL: backendUrl,
+  headers: { "Access-Control-Allow-Origin": frontendUrl },
+});
 
 export default {
-    //name: 'Fabian',
-    name: "Activities",
-    data () {
-        return {
-            accounts: [],
-            selectAccount: null,
-            activities: [],
-            selectActivity: null,
+  //name: 'Fabian',
+  name: "ScheduleActivity",
+  data() {
+    return {
+      accounts: [],
+      selectAccount: null,
+      activities: [],
+      selectActivity: null,
 
-            activityName: null,
-            description: null,
-            subcategory: null,
-            date: null,
-            startTime: null,
-            endTime: null,
-            //accountRoleId: null,
-            //activityName2: null,
-            capacity: null
-        };
-    },
-    mounted(){
-        this.createdAccounts();
-        this.createdActivities();
-    },
-    methods:{
+      activityName: null,
+      description: null,
+      subcategory: null,
+      date: null,
+      startTime: null,
+      endTime: null,
+      //accountRoleId: null,
+      //activityName2: null,
+      capacity: null,
+    };
+  },
+
+  methods: {
+    async submitScheduleActivity() {
+      const newScheduledActivity = {
+        date: this.date,
+        startTime: this.startTime,
+        endTime: this.endTime,
+        instructorId: this.instructorId,
+        activityName: this.activityName,
+        capacity: this.capacity,
+      };
+      try {
         /*
-        formatDate(event){
-            const selectedDate = new Date(event.target.value);
-            const localDate = DateTime.fromJSDate(selectedDate).toLocal(); // Convert to local time zone
-            const formattedDate = localDate.toISODate(); 
-        },
-        */
-        async createdAccounts(){
-            try{
-                const response = await AXIOS.get('/accounts');
-                this.accounts = response.data.accounts; // or response.data
-            } catch(error){
-                console.error('Error fetching accounts', error.message);
-            }
-        },
-        async createdActivities(){
-            try{
-                const response = await AXIOS.get('/activities');
-                this.activities = response.data;
-            } catch(error){
-                console.error('Error fetching activities', error.message);
-            }
-        },
-        async submitProposeActivity(){
-            const newActivity = {
-                name: this.activityName,
-                description: this.description,
-                subcategory: this.subcategory
-            };
-            try{
-                const response = await AXIOS.post('/createActivity', newActivity);
-                this.createActivity.push(response.data);
-                this.clearInputs();
-            } catch(error){
-                console.error('Error creating activity', error.message);
-            }
-        },
-        async submitScheduleActivity(){
-            const newScheduledActivity = {
-                date: this.date,
-                startTime: this.startTime,
-                endTime: this.endTime,
-                instructorId: this.instructorId,
-                activityName: this.activityName,
-                capacity: this.capacity
-            };
-            try{
-                /*
                 const localDate = DateTime.fromJSDate(date).toLocal(); // Convert to local date
                 date = localDate.toISODate(); 
 
@@ -124,29 +106,40 @@ export default {
                 endTime = localEndTime.toISOTime(); 
                 */
 
-                const response = await AXIOS.post('/createScheduledActivity', newScheduledActivity);
-                this.createScheduledActivity.push(response.data);
-                this.clearInputs();
-            } catch(error){
-                console.error('Error creating scheduled activity', error.message);
-            }
-        },
-        clearInputs(){
-            this.description = null;
-            this.subcategory = null;
-            this.selectAccount = null;
-            this.selectActivity = null;
-            this.instructorId = null;
-            this.activityName = null;
-            this.date = null;
-            this.startTime = null;
-            this.endTime = null;
-            this.capacity = null;
-        }
-
-    }
+        const response = await AXIOS.post(
+          "/createScheduledActivity/" +
+            date +
+            "/" +
+            startTime +
+            "/" +
+            endTime +
+            "/" +
+            instructorId +
+            "/" +
+            activityName +
+            "/" +
+            capacity
+        );
+        console.log(response.data);
+      } catch (error) {
+        alert("Error creating scheduled activity");
+        this.clearInputs();
+      }
+    },
+    clearInputs() {
+      this.description = null;
+      this.subcategory = null;
+      this.selectAccount = null;
+      this.selectActivity = null;
+      this.instructorId = null;
+      this.activityName = null;
+      this.date = null;
+      this.startTime = null;
+      this.endTime = null;
+      this.capacity = null;
+    },
+  },
 };
-
 </script>
 
 <style scoped src="../../assets/main.css"></style>
